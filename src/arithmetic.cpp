b97d824f2723d5129d21bbd3f06e2040934b9e45
VladJ52 <vlad.jirnow@gmail.com>
2017-12-20 21:43:11 +0300

    Added tests

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,6 +1,7 @@
 // реализация функций и классов для вычисления арифметических выражений
 #include "..\include\arithmetic.h"
 #include <iostream>
+#include <cstring>
 
 Lexem::Lexem(const Lexem& s)
 {
@@ -220,7 +221,7 @@ bool iscorrect(const string &s)
 				cout << "Lost sign" << endl;
 				return false;
 			}
-			if ((c[i] == '.') && (((c[i + 1] <= '0') || (c[i + 1] >= '9')) || ((c[i - 1] <= '0') || (c[i - 1] >= '9'))))
+			if ((c[i] == '.') && (((c[i + 1] < '0') || (c[i + 1] >= '9')) || ((c[i - 1] < '0') || (c[i - 1] >= '9'))))
 			{
 				cout << "Wrong signs" << endl;
 				return false;
@@ -274,6 +275,7 @@ Stack<Lexem> convertstr(const string & c)
 	string string_parametr;
 	Lexem b;
 	int j = 1;
+	int k = 1;
 	bool f = false;
 	if (s[1] == '-')
 	{
@@ -287,7 +289,8 @@ Stack<Lexem> convertstr(const string & c)
 		else
 		{
 			if ((((s[i] >= 'a') && (s[i] <= 'z')) || ((s[i] >= 'A') && (s[i] <= 'Z'))) || ((s[i] <= '9') && (s[i] >= '0') && (string_parametr != "")))
-				string_parametr = string_parametr + s[i];
+				for (int i = 0; i < len; i++)
+					string_parametr = string_parametr + s[i];
 			else
 			{
 				if (string_parametr != "")
